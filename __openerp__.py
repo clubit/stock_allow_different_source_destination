f9912f44383d8c23dd9d6ba7dfb4a385407bcfb2
Jan Vereecken <janvereecken@clubit.be>
2015-07-22 18:42:45 +0200

    ADDED: Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Stock Allow Diffferent Source Destination Location',
+    'summary': 'Standard Odoo doesn\'t allow to have different source destination location in the picking. This module monkey patches this behaviour.',
+    'version': '1.0',
+    'category': 'Warehouse',
+    'author': 'Clubit BVBA',
+    'website': 'http://www.clubit.be',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
